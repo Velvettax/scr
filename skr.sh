797062808d22aed58f2bbb528cba6ff81abd9c87
Velvettax <amad.motzart@gmail.com>
2020-09-13 23:09:26 +0200

    Update skr.sh

--- skr.sh
+++ skr.sh
@@ -6,4 +6,4 @@ sudo apt --yes --force-yes install software-properties-common
 sudo add-apt-repository --yes ppa:jonathonf/gcc-7.1
 sudo apt --yes --force-yes install git build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev gcc-7 g++-7
 
-wget -O - https://raw.githubusercontent.com/Velvettax/scr/master/a.sh
+wget -O - https://raw.githubusercontent.com/Velvettax/scr/master/a.sh |sudo bash
