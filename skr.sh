e3c18e5654c63f650d11b898d607fe40b149f0d7
Velvettax <amad.motzart@gmail.com>
2020-09-13 23:00:13 +0200

    Create skr.sh

---
+++ skr.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update
+sudo apt-get --yes --force-yes install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake
+sudo apt --yes --force-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+sudo apt --yes --force-yes install software-properties-common
+sudo add-apt-repository --yes ppa:jonathonf/gcc-7.1
+sudo apt --yes --force-yes install git build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev gcc-7 g++-7
+
+wget -O - https://raw.githubusercontent.com/Velvettax/scr/master/a.sh
